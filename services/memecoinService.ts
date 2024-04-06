ec6586c282f3ac18e318be04c7474db25fc7c0ea
akshatcoder-hash <akshatcoder@outlook.com>
2024-04-07 00:26:25 +0530

    taking out random memecoin

---
+++ services/memecoinService.ts
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+
+const memecoinsFilePath = path.join(__dirname, '../../data/memecoins.json');
+const memecoinsList: string[] = JSON.parse(fs.readFileSync(memecoinsFilePath, 'utf8')).memecoins;
+
+export const getRandomCoinSymbol = (): string => {
+  const randomIndex = Math.floor(Math.random() * memecoinsList.length);
+  return memecoinsList[randomIndex];
+};
