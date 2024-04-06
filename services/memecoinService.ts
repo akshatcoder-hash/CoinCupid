ab65e48a7fdb5631102cb0d5b518d4c9ca8428c7
akshatcoder-hash <akshatcoder@outlook.com>
2024-04-07 02:56:55 +0530

    load memecoins

--- services/memecoinService.ts
+++ services/memecoinService.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const memecoinsFilePath = path.join(__dirname, '../../data/memecoins.json');
+const memecoinsFilePath = path.join(__dirname, '../data/memecoins.json');
 const memecoinsList: string[] = JSON.parse(fs.readFileSync(memecoinsFilePath, 'utf8')).memecoins;
 
 export const getRandomCoinSymbol = (): string => {
